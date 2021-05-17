bc0058aac406871240e1067370165904e5a6ede3
elenamartincantero <72609858+elenamartincantero@users.noreply.github.com>
2021-05-17 15:05:21 +0200

    Add files via upload

---
+++ ex01.adb
@@ -0,0 +1,8 @@
+with Text_IO; use Text_IO;
+procedure ex01 is
+begin
+    Put_Line("---- Inicio del programa main ---- ");
+    delay 5.0;
+    Put_Line("---- Fin del programa main ---- ");
+end ex01;
+  
